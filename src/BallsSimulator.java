b9cfb0868ecb0b141a94b69a0d6360ca31b86598
gariela <arnaud.gariel@grenoble-inp.org>
2021-10-27 17:55:52 +0200

    commit arnaud marche avec p1p2p3p4

--- src/BallsSimulator.java
+++ src/BallsSimulator.java
@@ -4,10 +4,19 @@ import java.awt.Color;
 
 
 public class BallsSimulator implements Simulable {
-	Balls b = new Balls();
+
+	private Balls b;
+	private GUISimulator gui;
+
+	public BallsSimulator(GUISimulator gui, Balls b){
+
+			this.gui = gui;
+			this.b = b;
+	}
 
 	@Override
 	public void next() {
+
 		Random rand = new Random(); //instance of random class
 	  int upperbound = 50; //generate random values from 0-50
     int int_random1 = rand.nextInt(upperbound);
@@ -18,16 +27,19 @@ public class BallsSimulator implements Simulable {
 		gui.addGraphicalElement(new Oval((int)b.p2.getX(), (int)b.p2.getY(), Color.decode("#26b41f"), Color.decode("#26b41f"), 10)) ;
 		gui.addGraphicalElement(new Oval((int)b.p3.getX(), (int)b.p3.getY(), Color.decode("#b41f62"), Color.decode("#b41f62"), 10)) ;
 		gui.addGraphicalElement(new Oval((int)b.p4.getX(), (int)b.p4.getY(), Color.decode("#b4621f"), Color.decode("#b4621f"), 10)) ;
+
+
 	}
 
 	@Override
 	public void restart() {
+
 		this.b.reInit();
 		gui.reset();
 		gui.addGraphicalElement(new Oval((int)b.p1.getX(), (int)b.p1.getY(), Color.decode("#1f77b4"), Color.decode("#1f77b4"), 10)) ;
 		gui.addGraphicalElement(new Oval((int)b.p2.getX(), (int)b.p2.getY(), Color.decode("#26b41f"), Color.decode("#26b41f"), 10)) ;
 		gui.addGraphicalElement(new Oval((int)b.p3.getX(), (int)b.p3.getY(), Color.decode("#b41f62"), Color.decode("#b41f62"), 10)) ;
 		gui.addGraphicalElement(new Oval((int)b.p4.getX(), (int)b.p4.getY(), Color.decode("#b4621f"), Color.decode("#b4621f"), 10)) ;
-	}
 
+	}
 }
